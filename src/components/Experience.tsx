0152094f12d7befe55bd68e20d60265662e1bade
VihaanShah26 <vihaanshah2026@u.northwestern.edu>
2025-07-29 20:37:08 -0700

    fixed all image links

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -9,7 +9,7 @@ const experiences = [
     skills: ['Generative AI', 'RAG', 'Autonomous Databases', 'Semantic Search', 'OCI', 'Jira'],
     detailedDescription: "I'm working within the Security Central team responsible for handling security vulnerabilities within Oracle Cloud Infrastructure. I am working on multiple projects centered around improving developer efficiency and results through AI technologies.I built a RAG on top of existing models by utilizing FAQs about Oracle cloud services, history of Slack messages describing vulnerabilities and the deployed solution and Jira tickets issued by security scanners along with the associated fixes implemented. I used T-few fine tuning and dedicated AI clusters to efficiently complete this process using minimal resources. The resulting model has been integrated in a Slack channel with 8000+ engineers. I’m working on training an ML model to use attributes of a vulnerability to predict how long a particular process will take to complete scanning that vulnerability. This will help engineers prioritize vulnerabilities to fix. Lastly, I’m working on setting up an automation for data transformation and replication from an Autonomous Database to OCI OpenSearch, enabling faster and better search as well as visualization of data about security findings.",
     companyUrl: 'https://oracle.com',
-    logo: 'src/assets/logos/oracle.jpg'
+    logo: '/logos/oracle.jpg'
   },
   {
     company: 'Atomera',
@@ -22,7 +22,7 @@ const experiences = [
     Designed a robust and interactive app that connects to laboratory microscopes and processes the images from the slide being viewed under the microscope. Added multiple features and tools within the app that would allow end-to-end analysis of the images to perform quality checks and detect defects in semiconductor wafers. Utilised analytical techniques and artificial intelligence algorithms to automate the creation of statistical reports providing real-time information that aids the user in analysing the semiconductor wafers and making important decisions.
     `,
     companyUrl: 'https://atomera.com/',
-    logo: 'src/assets/logos/atomera.png'
+    logo: '/logos/atomera.png'
   },
   {
     company: 'Billed',
@@ -35,7 +35,7 @@ const experiences = [
     I am the sole backend developer responsible for setting up the entire infrastructure using AWS. My responsibilities include using OCR for converting snapshots of bills to text and parsing that text to find relevant information, making the app scalable using S3 and RDS to store user information, increasing security by facilitating payments on the backend and integrating it with the frontend using an API Gateway, and performing computation through lambda functions.
     `,
     companyUrl: "https://billed-app.com/",
-    logo: 'src/assets/logos/billed.jpeg'
+    logo: '/logos/billed.jpeg'
   },
   {
     company: 'FoodFight',
@@ -45,7 +45,7 @@ const experiences = [
     skills: ['RDBMS', 'MySQL', 'REST API', 'Event-Driven Processing', 'Real-Time Notification', 'AWS'],
     detailedDescription: "I worked as a Software Engineer focusing on the backend. The app facilitated live sports betting using food as a currency. I focused on collecting information about ongoing games using a sports API, generating outcomes for each game and storing them in a table in the database and designing a bet tracker to allow users to place bets and constantly check for status updates until an outcome has been decided and then informs the players about it. I utilised AWS RDS while working with the database and S3 while storing information about users and live bets.",
     companyUrl: "https://www.getfoodfight.com/",
-    logo: 'src/assets/logos/foodfight.png'
+    logo: '/logos/foodfight.png'
   },
   {
     company: 'Accenture',
@@ -55,7 +55,7 @@ const experiences = [
     skills: ['Python', 'Langchain', 'Stremlit', 'OpenAI API', 'MLOps', 'Tech Consulting'],
     detailedDescription: "I worked on a project consulting for a large consumer goods company to increase their productivity and manufacturing plant's overall efficiency. I designed an AI buddy to interact with the plant operators and provide them with essential information about the machinery, provide easy access to past data, evaluate and perform calculations on this past data to provide useful insights, and make future recommendations to prevent potential issues. This strongly developed my skills in Generative AI.",
     companyUrl: "https://www.accenture.com/us-en",
-    logo: 'src/assets/logos/accenture.png'
+    logo: '/logos/accenture.png'
   },
 ];
 
